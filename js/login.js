0705612a722a5b1d7b984dc7a3756cf63c9f4183
NeferUni <nefersneyderrp@ufps.edu.co>
2024-04-11 16:28:21 -0500

    Anexos

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault(); 
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+
+    if(username == "admin" && password == "1234") window.location.href = 'index.html';
+    else alert("Datos incorrectos");
+    
+});
